2512b0777779ff32602d014b1f52d2e7a6759434
Kazys Kersulis <kazimieras.kersulis@gmail.com>
2018-11-24 07:36:51 +0200

    modified imports

--- src/main/java/io/kazys/controller/TopicController.java
+++ src/main/java/io/kazys/controller/TopicController.java
@@ -4,8 +4,6 @@ import io.kazys.model.Topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
