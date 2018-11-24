4f535d6cf28313f2edf9f49015fddf13f0596705
Kazys Kersulis <kazimieras.kersulis@gmail.com>
2018-11-24 12:58:29 +0200

    added crud operations

--- src/main/java/io/kazys/controller/TopicController.java
+++ src/main/java/io/kazys/controller/TopicController.java
@@ -1,22 +1,43 @@
 package io.kazys.controller;
 
 import io.kazys.model.Topic;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import io.kazys.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping(value = "/topics", method = RequestMethod.GET)
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("java", "Java basics", "An introductory course on JAVA"),
-                new Topic("javascript", "Javascript fundamentals", "Intro to JavaScript"),
-                new Topic("spring", "Spring Framework", "Spring Framework introduction")
-        );
+        return topicService.getAllTopics();
     }
+
+    @RequestMapping(value = "/topics/{id}", method = RequestMethod.GET)
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(value = "/topics", method = RequestMethod.POST)
+    public void addTopic(@RequestBody Topic newTopic) {
+        topicService.addTopic(newTopic);
+    }
+
+    @RequestMapping(value = "/topics/{id}", method = RequestMethod.PUT)
+    public void updateTopic(@RequestBody Topic updatedTopic, @PathVariable String id) {
+        topicService.updateTopic(id, updatedTopic);
+    }
+
+    @RequestMapping(value = "/topics/{id}", method = RequestMethod.DELETE)
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
 
 }
